9721c6b1d490992e382475b5a5edfbd7a64e0902
Danmer Ccoscco <danmerccoscco@gmail.com>
2021-02-08 22:59:49 -0500

    headers faltantes

--- index.js
+++ index.js
@@ -51,6 +51,18 @@ app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');;
 })
 
+app.use(function (req, res, next) {
+
+    res.setHeader('Access-Control-Allow-Origin', '*');
+
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 app.post('/emit/:channel', cors(corsOptions),(req, res) => {
 
     var reqchannel = req.params.channel.split(',')
